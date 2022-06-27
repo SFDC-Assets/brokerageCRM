5a3d7a25829d7c5b46246a59900e60ebe6b1fd36
zhou059 <zhou059@gmail.com>
2022-06-26 22:14:58 -0700

    added commission statement

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #create scratch org
-sfdx force:org:create -s -f config/project-scratch-def.json -d 14 -s -w 60
+sfdx shane:org:create -f config/project-scratch-def.json -s -d 14 --userprefix broker -o force.demo
 
 sfdx force:package:install --package=04t1E000000jb9R --wait 15
 sfdx force:package:install --package=04t1E000001Iql5 --wait 2
@@ -19,8 +19,7 @@ sfdx force:apex:execute -f dparemoteSites.cls
 #Puppeteer to set OmniStudio Metadata to TRUE via UI  and enable the  standard runtime
 node run.js
 
-#Sample metadata examples into the org - basic hello world.
-sfdx force:source:deploy -m OmniUiCard,OmniScript,OmniIntegrationProcedure,OmniDataTransform
-
 #open says me.
 sfdx force:org:open
+
+# sfdx automig:dump -u michellelewis@michellelewis-20210819.demo -o Commission_Statement__c
